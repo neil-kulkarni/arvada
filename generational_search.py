505b728bb5c0fb56514a63c917b37ed8663a5d67
Neil Kulkarni <neil.kulkarni@berkeley.edu>
2020-02-04 16:19:14 -0800

    Changes from meeting on 2/4

--- generational_search.py
+++ generational_search.py
@@ -7,6 +7,7 @@ MAX_NONTERMINALS = 3
 NONTERMINALS = ["T" + str(i) for i in range(0, MAX_NONTERMINALS)]
 NUM_RULES = 4
 MAX_RHS_LEN = 3
+NUM_PERTURB = 3
 
 # Global Logging
 LOG = []
@@ -23,7 +24,8 @@ def setup(from_grammar=None):
         LOG = list(from_grammar.log)
         INDEX = 0
         REPLAY_MODE = True
-        perturb_log()
+        for _ in range(NUM_PERTURB):
+            perturb_log()
 
 def generate_grammar():
     start = generate_nonterminal()
@@ -69,6 +71,8 @@ def perturb_log():
     log_index = random.randint(0, len(LOG) - 1)
     (id, n, choice) = LOG[log_index]
     new_choice = random.randint(0, n - 1)
+    while new_choice == choice:
+        new_choice = random.randint(0, n - 1)
     if id == "loc5":
         rule_indices = [i - 1 for i, entry in enumerate(LOG) if entry[0] == "loc3"]
         if new_choice > choice:
@@ -93,7 +97,6 @@ def perturb_log():
             last = rule[-2:] * (new_choice - choice)
             rule = rule + last
         LOG = before + rule + after
-        pass
     elif id == "loc4" and choice != new_choice:
         # Generate a terminal instead of a nonterminal or vice versa
         next_entry = LOG[log_index + 1]
@@ -103,14 +106,27 @@ def perturb_log():
             LOG[log_index + 1] = ("loc1", len(ALPHABET), random.randint(0, len(ALPHABET) - 1))
     LOG[log_index] = (id, n, new_choice)
 
+def generate_negative_example():
+    example = ""
+    n_chars = random.randint(1, 10)
+    for _ in range(n_chars):
+        if random.randint(0, 1) == 1:
+            example += "a"
+        else:
+            example += "b"
+    if example in positive_examples:
+        return generate_negative_example()
+    else:
+        return example
+
 # Examples
 positive_examples = ['b' + 'a'*i for i in range(10)]
-negative_examples = ['a' + 'b'*i for i in range(10)]
+negative_examples = [generate_negative_example() for i in range(10)]
 
 # Score maximizing vector. Indices correspond to objectives:
 # 1. Accuracy (Positive)    2. Accuracy (Negative)  3. Size
-SCORE_VEC = [0] * 3
-MAX_ITERS = 10
+SCORE_VEC = [(0, None)] * 3
+MAX_ITERS = 10000
 
 def accuracy_score(grammar):
     try:
@@ -142,27 +158,41 @@ def size_score(grammar):
     for rule_start, rule in grammar.rules.items():
         for body in rule.bodies:
             total_rule_size += 1 + len(body)
-    return 1 / total_rule_size
-
-def print_grammar(grammar):
-    p_score, n_score = accuracy_score(prev_grammar)
-    s_score = size_score(prev_grammar)
-    print('Grammar:\n%s\n\nLog:\n%s\n\nScores:\n%s\n' % (prev_grammar, prev_grammar.log, (p_score, n_score, s_score)))
-    print('\n===========\n\n')
+    return 4 / total_rule_size
 
 setup()
-prev_grammar, prev_log = generate_grammar()
-prev_grammar.log = prev_log
-print_grammar(prev_grammar)
+grammar, log = generate_grammar()
+grammar.log = log
+p_score, n_score = accuracy_score(grammar)
+s_score = size_score(grammar)
+SCORE_VEC = [(p_score, grammar), (n_score, grammar), (s_score, grammar)]
 
 iterations = 0
 while iterations < MAX_ITERS:
-    setup(prev_grammar)
-    next_grammar, next_log = generate_grammar()
-    next_grammar.log = next_log
-    print_grammar(next_grammar)
-    prev_grammar = next_grammar
+    prev_grammar_index = random.randint(0, 3)
+    if prev_grammar_index < 3:
+        prev_grammar = SCORE_VEC[prev_grammar_index][1]
+        setup(prev_grammar)
+        grammar, log = generate_grammar()
+    else:
+        setup()
+        grammar, log = generate_grammar()
+    grammar.log = log
+    p_score, n_score = accuracy_score(grammar)
+    s_score = size_score(grammar)
+    if p_score > SCORE_VEC[0][0]:
+        SCORE_VEC[0] = (p_score, grammar)
+    if n_score > SCORE_VEC[1][0]:
+        SCORE_VEC[1] = (n_score, grammar)
+    if s_score > SCORE_VEC[2][0]:
+        SCORE_VEC[2] = (s_score, grammar)
+    print(iterations, [e[0] for e in SCORE_VEC])
     iterations += 1
+
+for score, grammar in SCORE_VEC:
+    print(grammar)
+    print(score)
+    print()
 
 # Grammar creation and perturbance example
 # setup()
