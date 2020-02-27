f082cbbf1fc006091866d77cdb8cfb7446ce06e4
Neil Kulkarni <neil.kulkarni@berkeley.edu>
2020-02-27 12:51:04 -0800

    added ability to input grammar and max iters from commandline

--- generational_search.py
+++ generational_search.py
@@ -1,5 +1,6 @@
-import random
+import random, sys, os
 from score import Scorer
+from input import parse_input
 from grammar import Grammar, Rule
 from generator import GrammarGenerator
 
@@ -20,36 +21,43 @@ def generate_negative_example():
 
 positive_examples = ['b' + 'a'*i for i in range(10)]
 negative_examples = [generate_negative_example() for i in range(10)]
-
-# Configuration Options
-TERMINALS = ['"a"', '"b"']
-NONTERMINALS = ["t" + str(i) for i in range(0, 3)]
-CONFIG = {'TERMINALS':TERMINALS, 'NONTERMINALS':NONTERMINALS, 'NUM_RULES':4, 'MAX_RHS_LEN':3}
 DATA = {'positive_examples':positive_examples, 'negative_examples':negative_examples}
-MAX_ITERS = 1000000
 
-# Generate initial grammar and score it
-gen = GrammarGenerator(CONFIG)
-grammar = gen.generate_grammar()
-scorer = Scorer(CONFIG, DATA, grammar, gen)
-scorer.score(grammar, gen)
+def main(file_name, max_iters):
+    # Generate configuration options and initial grammar
+    CONFIG, gen, grammar = parse_input(file_name)
+    MAX_ITERS = max_iters
 
-# Main Program Loop
-iterations = 0
-while iterations < MAX_ITERS:
-    good_grammar, good_gen = scorer.sample_grammar()
-    new_gen = good_gen.copy()
-    new_gen.mutate()
-    new_grammar = new_gen.generate_grammar()
-    scorer.score(new_grammar, new_gen)
-    print('Iters:', iterations, '\tScores:', [v[0] for v in scorer.score_map.values()])
-    iterations += 1
+    # Generate scorer object and score initial grammar
+    scorer = Scorer(CONFIG, DATA, grammar, gen)
+    scorer.score(grammar, gen)
 
-print('\n\n====== RESULTS ======\n\n')
-for category in scorer.score_map:
-    score, grammar, gen = scorer.score_map[category]
-    print('Category:', category)
-    print('Grammar:')
-    print(grammar)
-    print('Score:', score)
-    print()
+    # Main Program Loop
+    iterations = 0
+    while iterations < MAX_ITERS:
+        good_grammar, good_gen = scorer.sample_grammar()
+        new_gen = good_gen.copy()
+        new_gen.mutate()
+        new_grammar = new_gen.generate_grammar()
+        scorer.score(new_grammar, new_gen)
+        print('Iters:', iterations, '\tScores:', [v[0] for v in scorer.score_map.values()])
+        iterations += 1
+
+    print('\n\n====== RESULTS ======\n\n')
+    for category in scorer.score_map:
+        score, grammar, gen = scorer.score_map[category]
+        print('Category:', category)
+        print('Grammar:')
+        print(grammar)
+        print('Score:', score)
+        print()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3 or not os.path.exists(sys.argv[1]):
+        print('Usage: python3 generational_search.py <input_file> <max_iters>')
+    else:
+        try:
+            max_iters = int(sys.argv[2])
+            main(sys.argv[1], max_iters)
+        except:
+            print('Usage: python3 generational_search.py <input_file> <max_iters>')
---
+++ input.py
@@ -0,0 +1,40 @@
+import json
+from generator import *
+
+def parse_input(file_name):
+    # Load the JSON dictionary
+    f = open(file_name, 'r')
+    j = f.read()
+    d = json.loads(j)
+
+    # Create configuration options and grammar
+    config = d['config']
+    gen, grammar = generator_from_dict(d['grammar'], config)
+
+    # Clean the terminals configuration option to double quote terminals
+    clean_terminals(config)
+    return config, gen, grammar
+
+def clean_terminal(terminal):
+    return '"%s"' % terminal
+
+def clean_terminals(config):
+    config['TERMINALS'] = [clean_terminal(t) for t in config['TERMINALS']]
+
+def generator_from_dict(grammar_dict, config):
+    start, rules = grammar_dict['start'], grammar_dict['rules']
+    grammar_node = GrammarNode(config, start, [])
+    for rule in rules:
+        rule_start, rule_bodies = rule['start'], rule['bodies']
+        for rule_body in rule_bodies:
+            rule_node = RuleNode(config, rule_start, [])
+            for symbol in rule_body:
+                if symbol in config['TERMINALS']:
+                    symbol_node = SymbolNode(config, clean_terminal(symbol), True)
+                else:
+                    symbol_node = SymbolNode(config, symbol, False)
+                rule_node.children.append(symbol_node)
+            grammar_node.children.append(rule_node)
+    grammar_gen = GrammarGenerator(config, grammar_node)
+    grammar = grammar_gen.generate_grammar()
+    return grammar_gen, grammar
