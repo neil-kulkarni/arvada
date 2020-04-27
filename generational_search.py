333e8cd5b995c9b657ff2f1b30d70439c2741c8d
Caroline Lemieux <caro.lemieux@gmail.com>
2020-04-27 15:08:27 -0700

    logging

--- generational_search.py
+++ generational_search.py
@@ -5,7 +5,7 @@ from parse_tree import ParseTree
 from grammar import Grammar, Rule
 from generator import GrammarGenerator
 
-def main(file_name, max_iters):
+def main(file_name, log_file, max_iters):
     # Generate configuration options and oracle grammar
     CONFIG, ORACLE_GEN, ORACLE = parse_input(file_name)
     POS_EXAMPLES, NEG_EXAMPLES = CONFIG['POS_EXAMPLES'], CONFIG['NEG_EXAMPLES']
@@ -15,8 +15,9 @@ def main(file_name, max_iters):
     # Generate positive examples
     oracle_parse_tree = ParseTree(ORACLE_GEN)
     positive_examples = oracle_parse_tree.sample_strings(POS_EXAMPLES, MAX_TREE_DEPTH)
+    print(len(positive_examples))
     negative_examples = ORACLE.sample_negatives(NEG_EXAMPLES, TERMINALS, MAX_NEG_EXAMPLE_SIZE)
-    DATA = {'positive_examples':positive_examples, 'negative_examples':negative_examples}
+    DATA = {'positive_examples': positive_examples, 'negative_examples': negative_examples}
 
     # Generate intial grammar and score it
     gen = GrammarGenerator(CONFIG)
@@ -24,33 +25,47 @@ def main(file_name, max_iters):
     scorer = Scorer(CONFIG, DATA, grammar, gen)
     scorer.score(grammar, gen)
 
+    def print_results(logfile):
+        f = open(logfile, "w")
+        print("Positive Examples:", file=f)
+        for pos in positive_examples:
+            print(pos, file=f)
+        print("Negative Examples:", file=f)
+        for neg in negative_examples:
+            print(neg, file=f)
+        print('\n\n====== RESULTS ======\n\n', file=f)
+        for category in scorer.score_map:
+            score, grammar, gen = scorer.score_map[category]
+            print('Category:', category, file=f)
+            print('Grammar:', file=f)
+            print(grammar, file=f)
+            print('Score:', score, file=f)
+            print(file=f)
+
+
     # Main Program Loop
     iterations = 0
     while iterations < MAX_ITERS:
+        if iterations % 500 == 0:
+            print_results(log_file)
         good_grammar, good_gen = scorer.sample_grammar()
         new_gen = good_gen.copy()
         new_gen.mutate()
         new_grammar = new_gen.generate_grammar()
         scorer.score(new_grammar, new_gen)
-        print('Iters:', iterations, '\tScores:', [v[0] for v in scorer.score_map.values()])
+        print('Iters:', iterations, '\tScores:', ", ".join(["{:.2f}".format(v[0]) for v in scorer.score_map.values()]), end='\r')
         iterations += 1
 
-    print('\n\n====== RESULTS ======\n\n')
-    for category in scorer.score_map:
-        score, grammar, gen = scorer.score_map[category]
-        print('Category:', category)
-        print('Grammar:')
-        print(grammar)
-        print('Score:', score)
-        print()
+    print_results(log_file)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3 or not os.path.exists(sys.argv[1]):
-        print('Usage: python3 generational_search.py <input_file> <max_iters>')
+    if len(sys.argv) != 4 or not os.path.exists(sys.argv[1]):
+        print('Usage: python3 generational_search.py <input_file> <log_file> <max_iters>')
     else:
         try:
-            max_iters = int(sys.argv[2])
+            log_file = sys.argv[2]
+            max_iters = int(sys.argv[3])
         except:
-            print('Usage: python3 generational_search.py <input_file> <max_iters>')
+            print('Usage: python3 generational_search.py <input_file> <log_file> <max_iters>')
             exit()
-        main(sys.argv[1], max_iters)
+        main(sys.argv[1], log_file, max_iters)
