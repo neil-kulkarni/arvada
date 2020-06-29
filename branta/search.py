5512ca4e5baa0f0221570ed5a985b271a2245454
Caroline Lemieux <caro.lemieux@gmail.com>
2020-06-29 11:28:01 -0700

    prevent mutations from clobbering the parent

--- branta/search.py
+++ branta/search.py
@@ -77,19 +77,35 @@ def mutate_bubble(grammar: Grammar) -> Grammar:
     Bubble up some subsequence of nonterminals
     """
 
-    mutant = grammar.copy()
+    #print("===BUBBLE===")
+    mutant = Grammar(grammar.rules['start'].bodies[0][0])
     sequences : Set[Tuple[str]] = set()
-    for rule in mutant.rules.values():
+    for rule in grammar.rules.values():
         for body in rule.bodies:
             sequences.update(strict_subsequences(body))
-    replace_seq = random.choice(list(sequences))
-    replace_name = new_nonterminal(mutant.rules.keys())
+    if len(sequences) == 0:
+        for rule in grammar.rules.values():
+            rule_bodies = rule.bodies
+            new_rule = Rule(rule.start)
+            for body in rule_bodies:
+                new_rule.add_body(body[:])
+            mutant.add_rule(new_rule)
+        return mutant
 
-    for rule in mutant.rules.values():
-        rule.bodies = [replace_all(body, replace_seq, replace_name) for body in rule.bodies]
-        rule.cache_valid = False
+    replace_seq = random.choice(list(sequences))
+    replace_name = new_nonterminal(grammar.rules.keys())
+
+    for rule in grammar.rules.values():
+        rule_bodies = [replace_all(body[:], replace_seq, replace_name) for body in rule.bodies]
+        new_rule = Rule(rule.start)
+        for body in rule_bodies:
+            new_rule.add_body(body)
+        mutant.add_rule(new_rule)
 
     mutant.add_rule(Rule(replace_name).add_body(list(replace_seq)))
+    #print(f"Bubbled up {replace_name}->{replace_seq}")
+    #print("keys before: ", list(grammar.rules.keys()))
+    #print("keys after: ", list(mutant.rules.keys()))
     return mutant
 
 def mutate_coalesce(grammar: Grammar) -> Grammar:
@@ -97,31 +113,36 @@ def mutate_coalesce(grammar: Grammar) -> Grammar:
     Coalesce some elements together.
     """
 
-    mutant = grammar.copy()
-    nonterminals = list(mutant.rules.keys())
-    terminals = [elem for rule in mutant.rules.values() for body in rule.bodies for elem in body if elem not in nonterminals]
+    #print("===COALESCE===")
+    mutant = Grammar(grammar.rules['start'].bodies[0][0])
+
+    nonterminals = list(grammar.rules.keys())
+    terminals = [elem for rule in grammar.rules.values() for body in rule.bodies for elem in body if elem not in nonterminals]
     nonterminals.remove('start')
-    print(nonterminals)
-    print(terminals)
 
     all = nonterminals
     coalesce_num = random.choice(range(2,min(6, len(all))))
     to_coalesce = random.sample(all, coalesce_num)
 
     replacer_id = new_nonterminal(nonterminals)
-    for rule in mutant.rules.values():
+    for rule in grammar.rules.values():
         # Replace all the things in "to_coalesce" with what we're coalescing
-        rule.bodies = [[replacer_id if e in to_coalesce else e for e in body] for body in rule.bodies]
-        rule.cache_valid = False
+        rule_bodies = [[replacer_id if e in to_coalesce else e for e in body] for body in rule.bodies]
+        new_rule = Rule(rule.start)
+        for body in rule_bodies:
+            new_rule.add_body(body)
+        mutant.add_rule(new_rule)
 
     replacer_rules = []
     for coalescee in to_coalesce:
-        if coalescee in mutant.rules:
-            rule = mutant.rules.pop(coalescee)
-            replacer_rules.extend(rule.bodies)
+        if coalescee in grammar.rules:
+            rule = grammar.rules[coalescee]
+            for body in rule.bodies:
+                replacer_rules.append(body[:])
     replacer = Rule(replacer_id)
     replacer.bodies = replacer_rules
     mutant.add_rule(replacer)
+    #print(f"Coalesced up {to_coalesce}->{replacer_id}")
 
     return mutant
 
@@ -130,6 +151,8 @@ def mutate_alternate(grammar: Grammar) -> Grammar:
     Allow some elements to alternate.
     """
     mutant = grammar.copy()
+    #print("===ALTERNATE===")
+    #print(list(grammar.rules.keys()))
 
     nonterminals = list(mutant.rules.keys())
     terminals = [elem for rule in mutant.rules.values() for body in rule.bodies for elem in body if elem not in nonterminals]
@@ -150,33 +173,50 @@ def mutate_alternate(grammar: Grammar) -> Grammar:
     new_body[alternate_idx] = alternate
     mutant.rules[rule_key].add_body(new_body)
 
+    #print("keys before: ", nonterminals)
+    #print("keys after: ", list(mutant.rules.keys()))
+
+    #print(f"Alternated at {rule_key}->{mutant.rules[rule_key].bodies[body_idx][alternate_idx]}|{alternate}")
     return mutant
 
 def mutate_repeat(grammar: Grammar) -> Grammar:
     """
     Allow some element to repeat
     """
-    mutant = grammar.copy()
+    mutant = Grammar(grammar.rules['start'].bodies[0][0])
+    #print("===REPEAT===")
 
-
-    nonterminals = list(mutant.rules.keys())
+    nonterminals = list(grammar.rules.keys())
     nonterminals.remove('start')
 
-    total_num_rules = len([body for nonterm in nonterminals for body in mutant.rules[nonterm].bodies])
-    weights = [len(mutant.rules[nonterm].bodies)/ total_num_rules for nonterm in nonterminals]
+    total_num_rules = len([body for nonterm in nonterminals for body in grammar.rules[nonterm].bodies])
+    weights = [len(grammar.rules[nonterm].bodies)/ total_num_rules for nonterm in nonterminals]
 
     rule_key = random.choices(nonterminals, weights, k=1)[0]
-    body_idx = random.choice(range(len(mutant.rules[rule_key].bodies)))
-    repeat_idx = random.choice(range(len(mutant.rules[rule_key].bodies[body_idx])))
+    body_idx = random.choice(range(len(grammar.rules[rule_key].bodies)))
+    repeat_idx = random.choice(range(len(grammar.rules[rule_key].bodies[body_idx])))
 
-    repeat_elem = mutant.rules[rule_key].bodies[body_idx][repeat_idx]
+    repeat_elem = grammar.rules[rule_key].bodies[body_idx][repeat_idx]
     repeater_name = new_nonterminal(nonterminals)
-    mutant.rules[rule_key].bodies[body_idx][repeat_idx] = repeater_name
-    mutant.rules[rule_key].cache_valid = False
+
+    #print(f"In rule {rule_key} -> {grammar.rules[rule_key].bodies[body_idx]} repeating {repeat_elem} into {repeater_name}")
+    for rule in grammar.rules.values():
+        new_rule = Rule(rule.start)
+        if rule.start == rule_key:
+            for i in range(len(rule.bodies)):
+                if i == body_idx:
+                    bod = rule.bodies[i]
+                    new_rule.add_body(bod[:repeat_idx] + [repeater_name] + bod[repeat_idx+1:])
+                else:
+                    new_rule.add_body(rule.bodies[i][:])
+        else:
+            for body in rule.bodies:
+                new_rule.add_body(body[:])
+        mutant.add_rule(new_rule)
 
     mutant.add_rule(Rule(repeater_name).add_body([repeat_elem]).add_body([repeat_elem, repeater_name]))
 
-    return grammar
+    return mutant
 
 def minimize(grammar: Grammar) -> None:
     pass
@@ -209,12 +249,30 @@ def search(oracle: Oracle, guides: List[str], positives: List[str], negatives: L
         """
         nonlocal cur_gram_id
         nonlocal minimum_score
+
         cur_gram_id += 1
         num_mutations = random.choice([1, 2, 4, 8, 16])
         mutant = parent_grammar
+        before_score = scorer.score(parent_grammar)
         for i in range(num_mutations):
+            parent_grammar.cached_str_valid = False
+            # print("At topmost of iteration: ", mutant.rules.keys())
+
             mutate_func = random.choice([mutate_bubble, mutate_coalesce, mutate_alternate, mutate_repeat])
             mutant = mutate_func(mutant)
+
+            for rule in mutant.rules.values():
+                rule.cache_valid = False
+            mutant.cached_str_valid = False
+
+
+            #print("score is ", scorer.score(mutant))
+
+            # print("At bottom of iteration: ", mutant.rules.keys())
+
+
+
+        #print("!!!!!done@!!!!!")
         mutant_score = scorer.score(mutant)
         if mutant_score > minimum_score:
             add_to_population(mutant, mutant_score, cur_gram_id)
@@ -226,26 +284,32 @@ def search(oracle: Oracle, guides: List[str], positives: List[str], negatives: L
         nonlocal minimum_score
         nonlocal population
         assert(score > minimum_score)
-        minimize(grammar)
+        #minimize(grammar)
 
         insert_idx = 0
         # TODO: assert that everything stays sorted
         for elem_grammar, elem_id, elem_score in population:
             # This ensures that older grammars will be popped off before new ones
+            #print(elem_score)
             if elem_score > score:
-                population.insert(insert_idx, (grammar, grammar_id, score))
                 break
             insert_idx += 1
+        population.insert(insert_idx, (grammar, grammar_id, score))
+        #print("[POP] is of size", len(population))
 
         while len(population) > POPULATION_SIZE:
             population.pop(0)
+        #print("[POP] is of size", len(population))
+        minimum_score = population[0][2]
 
     #################### END HELPERS #######################
 
-    while not score_is_good(minimum_score):
+    for i in range(0, 30):
         current_parent, parent_id, score = choose_parent(population)
         fuzz_one(current_parent)
-        break
+        print(f"Current best score: {population[-1][2]}")
+
+    print(f"Current best score: {population[-1][2]}")
 
 
 
