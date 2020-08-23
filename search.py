56c5780b9070efb19c1e986dc0c9876af00727e6
Caroline Lemieux <caro.lemieux@gmail.com>
2020-08-22 20:50:17 -0700

    pickle grammar at the end of search

--- search.py
+++ search.py
@@ -92,87 +92,9 @@ def main_external(external_folder, log_file, fast = False):
         print(f'Time spent building + scoring grammar: {time.time() - start_time}s', file = f)
         print(f'Parse calls: {oracle.parse_calls}')
         print(f'Parse calls: {oracle.parse_calls}', file = f)
-
-def main(file_name, log_file):
-    start_time = time.time() # To compute elapsed time
-
-    # Generate configuration options and oracle grammar
-    ORACLE: Grammar
-    CONFIG, ORACLE = parse_input(file_name)
-    POS_EXAMPLES, NEG_EXAMPLES = CONFIG['POS_EXAMPLES'], CONFIG['NEG_EXAMPLES']
-    MAX_NEG_EXAMPLE_SIZE = CONFIG['MAX_NEG_EXAMPLE_SIZE']
-    TERMINALS, MAX_TREE_DEPTH = CONFIG['TERMINALS'], CONFIG['MAX_TREE_DEPTH']
-    GUIDE_EXAMPLES = CONFIG['GUIDE']
-
-    # Test to make sure that the oracle at least compiles, and throws an exception if not
-    try:
-        OR_PARSER = ORACLE.parser()
-        OR_PARSER = CachingOracle(OR_PARSER)
-    except Exception as e:
-        print(f"Oops! The Lark parser couldn't compile. Here's the error:\n {e.__str__()}\n")
-        print('Fix your grammar. Exiting now.')
-        exit(1)
-
-
-    # Generate guiding examples and corresponding ParseNodes
-    #guide_nodes = [[ParseNode(tok, True, []) for tok in ex.split()] for ex in GUIDE_EXAMPLES]
-    # TODO: this assumes there are no spaces in the inputs...
-    guide_nodes = [[ParseNode(tok, True, []) for tok in ex.replace(' ', '')] for ex in GUIDE_EXAMPLES]
-
-    # Create the log file and write positive and negative examples to it
-    # Also write the initial starting grammar to the file
-    with open(log_file, 'w+') as f:
-
-        # Build the starting grammars and test them for compilation
-        print('Building the starting grammar...'.ljust(50), end='\r')
-        start_time = time.time()
-        start_grammar: Grammar = build_start_grammar(OR_PARSER, guide_nodes)
-        try:
-            start_grammar.parser()
-            print('\n\nInitial Grammar Created:\n%s' % str(start_grammar), file=f)
-        except Exception as e:
-            print('\n\nInitial grammar does not compile! %s' % str(e), file=f)
-            print(start_grammar, file = f)
-            exit()
-        build_time = time.time() - start_time
-
-
-        # Score the start grammar we arrived at earlier, which will add it to the
-        # "interesting" set for future iterations
-
-        print('Scoring grammar....'.ljust(50), end='\r')
-        recall_set = start_grammar.sample_positives(100, 5)
-        precision_set = ORACLE.sample_positives(100, 5)
-
-        parser : Lark = start_grammar.parser()
-
-        recall_num = 0
-        print(f"Recall set (size {len(recall_set)}):", file=f)
-        for example in recall_set:
-            try:
-                print("   ", example, file=f)
-                OR_PARSER.parse(example)
-                recall_num += 1
-            except Exception as e:
-                continue
-
-        precision_num = 0
-
-        print(f"Precision set (size {len(precision_set)}):", file=f)
-        for example in precision_set:
-            try:
-                print("   ", example, file=f)
-                parser.parse(example)
-                precision_num += 1
-            except Exception as e:
-                continue
-
-        print(f'Precision: {precision_num/len(precision_set)}, Recall: {recall_num/len(recall_set)}', file=f)
-        print(f"Build_time: {build_time}")
-        print(f'Precision: {precision_num/len(precision_set)}, Recall: {recall_num/len(recall_set)}')
-        print(f'Time spent building grammar: {build_time}s', file = f)
-        print(f'Time spent building + scoring grammar: {time.time() - start_time}s', file = f)
-
+        print(f'Pickling grammar...')
+        import pickle
+        pickle.dump(start_grammar, open(log_file + ".gram", "wb"))
 
 
 if __name__ == '__main__':
@@ -182,7 +104,7 @@ if __name__ == '__main__':
         if not os.path.exists(sys.argv[2]):
             print(f"err: {sys.argv[2]} not found")
     elif sys.argv[1] == "internal":
-        main(sys.argv[2], sys.argv[3])
+        print("NO LONGER SUPPORTED")
     elif sys.argv[1] == "external":
         main_external(sys.argv[2], sys.argv[3], True)
     else:
